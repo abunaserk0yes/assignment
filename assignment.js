1a2d8f50ccd472d300ae03de89d89faa5b903285
Md Abu Naser Kayes <abunaserkayes3305@yahoo.com>
2021-01-22 20:09:42 +0600

    code

--- assignment.js
+++ assignment.js
@@ -1,54 +1,52 @@
+//Problem-1(Kilometer To Meter)
 
-
-//Problem-1
 function kilometerToMeter(kilometer) {
     var meter = kilometer / 1000;
     return meter;
+};
+
+// Problem-2(Budget Calculator)
+
+function budgetCalculator(watch, mobail, laptop) {
+    var watchCost = watch * 50;
+    var mobailCost = mobail * 100;
+    var laptopCost = laptop * 500;
+    var total = watchCost + mobailCost + laptopCost;
+    return total
+};
+
+// Problem-3(Night Costing)
+
+function hotelCost(night) {
+
+    var cost = 0;
+    if (night <= 10) {
+        cost = night * 100;
+    } else if (night <= 20) {
+        var firstCost = 10 * 100;
+        var remaingCost = night - 10;
+        var secondCost = remaingCost * 80;
+        cost = firstCost + secondCost;
+    } else {
+        firstCost = 10 * 100;
+        secondCost = 10 * 80;
+        remaingCost = night - 20;
+        var thirdCost = remaingCost * 50;
+        cost = firstCost + secondCost + thirdCost;
+    }
+
+    return cost;
 }
-console.log(kilometerToMeter(12000));
 
-// // Problem-2
-// function budgetCalculator(watch, mobail, laptop) {
-//     var watchCost = watch * 50;
-//     var mobailCost = mobail * 100;
-//     var laptopCost = laptop * 500;
-//     var total = watchCost + mobailCost + laptopCost;
-//     return total
-// }
+// Problem-4(Megafriend String length)
 
-// // Problem-3
-// function hotelCost(night) {
-
-//     var cost = 0;
-//     if (night <= 10) {
-//         cost = night * 100;
-//     } else if (night <= 20) {
-//         var firstCost = 10 * 100;
-//         var remaingCost = night - 10;
-//         var secondCost = remaingCost * 80
-//         cost = firstCost + secondCost;
-//     } else {
-//         firstCost = 10 * 100;
-//         secondCost = 10 * 80;
-//         remaingCost = night - 20;
-//         var thirdCost = remaingCost * 50;
-//         cost = firstCost + secondCost + thirdCost;
-//     }
-
-//     return cost;
-// }
-
-// // Problem-4
-
-// function megaFriend(friendsName){
-//     var mega = friendsName[0];
-//     for (var i =[0]; i < friendsName.length; i[0]++) {
-//         var element = friendsName[i];
-//         if (element.length > mega.length) {
-//             mega = element;
-//         } 
-//     } return mega;
-   
-// }
-
-
+function megaFriend(friendsName) {
+    var mega = friendsName[0];
+    for (var i = 0; i < friendsName.length; i++) {
+        var element = friendsName[i];
+        if (element.length > mega.length) {
+            mega = element;
+        }
+    }
+    return mega;
+}
